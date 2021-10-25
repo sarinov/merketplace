4d5805ed0beffe7ccbb14aafff1fc0c71cd0193d
Tatyana Poteshkina <57525148+IsaakWoof@users.noreply.github.com>
2021-10-25 12:42:11 +0600

    Code Formatting

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -4,36 +4,39 @@
 
 @section('main_content')
 
-<div class="container">
+<!-- start container -->
+    <div class="container">
 
-<center>
+        <center>
+
+            <form style="max-width: 600px; width: 100%; margin: 110px auto;"
+                <h3 class="text-center">
+                    Sign in
+                </h3>
+
+                <div class="form-group row mt-5">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputEmail" value="email@example.com"
+                            placeholder="email@example.com">
+                    </div>
+                </div>
+                
+                <div class="form-group row mt-5">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                    </div>
+                </div>
+                <button type="button" class="btn btn-primary mt-5">Sign in</button>
+            </form>
+
+        </center>
 
 
-<form  style="max-width: 600px; width: 100%; margin: 110px auto;">
-    <h3 class="text-center">
-        Sign in
-    </h3>
-
-    <div class="form-group row mt-5">
-      <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-      <div class="col-sm-10">
-        <input type="text"  class="form-control"  id="inputEmail" value="email@example.com" placeholder="email@example.com">
-      </div>
     </div>
-    <div class="form-group row mt-5">
-      <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-      <div class="col-sm-10">
-        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-      </div>
-    </div>
-    <button type="button" class="btn btn-primary mt-5">Sign in</button>
-  </form>
-        
-</center>
-
-
-</div>
+<!-- end container -->
 
 
 
-    @endsection
+@endsection
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,66 +1,82 @@
 @extends('layout')
 
-@section('title')Welcome @endsection
+@section('title')Single @endsection
 
 @section('main_content')
 
 
+<!-- start container -->
+    <div class="container">
+        <div class="product p-5 border rounded-3">
+            <div class="d-flex align-items-center">
 
-<div class="container">
-     <div class="product p-5 border rounded-3">
-        <div class="d-flex align-items-center">
-          <div class="flex-shrink-0">
-            <img src="https://image.shutterstock.com/image-illustration/img-file-document-icon-trendy-260nw-1407027353.jpg" width="250px" alt="...">
-          </div>
-          <div class="flex-grow-1 ms-3">
-            <h3 class="title" id="name_{{$products->id}}">{{$products->name}} </h3>
-            <p class="description lead" id="description_{{$products->id}}">{{$products->decription}} </p>
+                <div class="flex-shrink-0">
+                    <img src="https://image.shutterstock.com/image-illustration/img-file-document-icon-trendy-260nw-1407027353.jpg"
+                        width="250px" alt="...">
+                </div>
 
-          <div class=" d-flex">
-            <p class="price"  style="margin-right: 20px;"> price: <span id="price_{{$products->id}}" > {{$products->price}}</span> </p>
-            <p class="count"  style="margin-right: 20px;" >count: <span id="count_{{$products->id}}" >{{$products->count}}</span> </p> 
-        
-            <p class="like"  style="margin-right: 20px;">Like: <span id="like_{{$products->id}}">{{$products->like}}</span> </p>
-            <p class="dislike"  style="margin-right: 20px;">Dislike: <span id="dislike_{{$products->id}}" >{{$products->dislike}}</span> </p> 
+                <div class="flex-grow-1 ms-3">
+                    <h3 class="title" id="name_{{ $products->id }}">{{ $products->name }} </h3>
+                    <p class="description lead" id="description_{{ $products->id }}">{{ $products->decription }} </p>
+
+                    <div class=" d-flex">
+
+                        <p class="price" style="margin-right: 20px;"> price: <span id="price_{{ $products->id }}">
+                                {{ $products->price }}</span> </p>
+                        <p class="count" style="margin-right: 20px;">count: <span
+                                id="count_{{ $products->id }}">{{ $products->count }}</span> </p>
+
+                        <p class="like" style="margin-right: 20px;">Like: <span
+                                id="like_{{ $products->id }}">{{ $products->like }}</span> </p>
+                        <p class="dislike" style="margin-right: 20px;">Dislike: <span
+                                id="dislike_{{ $products->id }}">{{ $products->dislike }}</span> </p>
+
+                    </div>
+
+                    <div class="btn-group" role="group">
+
+                        <button onclick="delete_product({{ $products->id }})" type="button"
+                            class="btn border">Delete</button>
+                        <button onclick="update_cells({{ $products->id }})" id="update_cells_{{ $products->id }}"
+                            type="button" class="btn border">Update</button>
+                        <button onclick="like({{ $products->id }})" type="button" class="btn border">Like</button>
+                        <button onclick="dislike({{ $products->id }})" type="button"
+                            class="btn border">Dislike</button>
+                        <button onclick="increment({{ $products->id }})" type="button" class="btn border">Counte
+                            +</button>
+                        <!--  <button type="button" class="btn ">Update</button>
+                    <button type="button" class="btn ">Delete</button>
+                    <button type="button" class="btn ">Increment</button>
+                    <button type="button" class="btn ">Dicrement</button>
+                    <button type="button" class="btn ">Like</button>
+                    <button type="button" class="btn ">Dislike</button> -->
+
+
+                    </div>
+
+                </div>
 
             </div>
-          <div class="btn-group" role="group">
-          
-                <button onclick="delete_product({{$products->id}})" type="button" class="btn border">Delete</button>
-                  <button onclick="update_cells({{$products->id}})" id = "update_cells_{{$products->id}}" type="button" class="btn border">Update</button>
-                  <button onclick="like({{$products->id}})"type="button" class="btn border">Like</button>
-                  <button onclick="dislike({{$products->id}})"type="button" class="btn border">Dislike</button>
-                  <button onclick="increment({{$products->id}})"type="button" class="btn border">Counte +</button>
-            <!--  <button type="button" class="btn ">Update</button>
-            <button type="button" class="btn ">Delete</button>
-            <button type="button" class="btn ">Increment</button>
-            <button type="button" class="btn ">Dicrement</button>
-            <button type="button" class="btn ">Like</button>
-            <button type="button" class="btn ">Dislike</button> -->
-          
-          
-          </div>
+
         </div>
-</div>
- 
-</div>
+    </div>
+<!-- end container -->
+
+
+
+
+
+
+    </div>
+    </div>
     </div>
 
-    
+
+
+    </div>
 
 
 
 
-  </div>
-</div>
-     </div>
 
-
-
-</div>
-
-
-
-   
-
-    @endsection
+@endsection
